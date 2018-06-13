ee043e5c86e3dfecc9af794b8874b48705b65e73
xinshangshangxin <xinshangshangxin@gmail.com>
2018-06-13 09:38:06 +0800

    feat: update error

--- server/src/config/error.js
+++ server/src/config/error.js
@@ -1,5 +1,5 @@
 import buildError from '@ofa2/ofa2-error';
 
 global.Errors = buildError({
-  UnknownError: { code: 1, message: 'unknown error, need feedback' },
+  Unknown: 'unknown error, need feedback',
 });
